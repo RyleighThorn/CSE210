3bba8ecc5dc43f802dd03c73de105ad8619b8f49
RyleighTHORN <medallionkeepers@gmail.com>
2026-09-21 11:00:16 -0600

    progect one

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        string firstname;
+        string lastname;
+        Console.Write("please enter your first name: ");
+        firstname = Console.ReadLine();
+        Console.Write("please enter your last name: ");
+        lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
+        int x = 10;
+
+        if (x == 10){
+            Console.WriteLine("X yay");
+            Console.WriteLine("Y also yay");
+        }
+        
+
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.Write("What is your percentage grade: ");
+        string user_grade = Console.ReadLine();
+        int x = int.Parse(user_grade);
+
+        if (x >= 90 && x <= 100)
+        {
+            letter = "A";
+        } else if (x >= 81 && x <= 89)
+        {
+            letter = "B";
+        } else if (x >= 71 && x <= 79)
+        {
+            letter = "C";
+        } else if (x >= 61 && x <= 69)
+        {
+            letter = "D";
+        } else if (x >= 0 && x < 60)
+        {
+            letter = "F";
+        }
+        else{
+            Console.Write("Not usable answer");
+        }
+        Console.WriteLine($" Your grade is {letter}");
+        if (x >= 70)
+        {
+            Console.Write("Congrats you passed!");
+        }
+        else
+        {
+            Console.Write("You failed, better luck next time");
+        }
     }
 }
